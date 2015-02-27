0b19bed3452d78a6b9f29dc84d4080961a5fd529
Julia Notar <jnotar@ucla.edu>
2015-02-27 10:03:03 +0000

    Update script to be compatible with filenames and outputs of echino_id.py

--- download_fasta.py
+++ download_fasta.py
@@ -1,24 +1,30 @@
+#! /usr/bin/python
+
+from __future__ import division
+
 #This function downloads the content of fasta files from individual identifiers (GI) from genbank
 #save the output string as a text file
 
-import Bio #import Biopython
+#Import important packages: regex, Biopython, & Entrez (from Biopy)
+import re
+import Bio
 from Bio import Entrez
 
-Entrez.email = "jnotar@ucla.edu" #
+Entrez.email = "jnotar@ucla.edu" #tell Entrez who you are
 
 #open my idlist from my saved file of GI #s
-#idlist = open("genbank_idlist.txt").read()
-
-#this is my test list
-idlist = ["47551060", "346418465"] 
+idlist = open("echino_ids.txt").read() #open list & read into one string 
+idlist = re.findall(r"\'(\d+)\'", idlist) #get rid of other characters by cutting out each id and putting it in a separate entry in a list
 
 def function(list): 
     for id in idlist:
-        file = open("{}.txt".format(id), "w")
+        file = open("{}.fasta".format(id), "w")
+        	#change "{}.fasta" to "{}.txt" if you want to save it as a text file. But why? 
         handle = Entrez.efetch(db = "nucleotide", id = "{}".format(id), rettype = "fasta", retmode = "text")
-        file.write(handle.read())
-        file.close()
+        	#tell Entrez to fetch entries from nucleotide, that match id from idlist, fasta files only
+        file.write(handle.read()) #write to file object
+        file.close() #close file
 
-function(idlist)
+function(idlist) #run the function on the idlist of GIs
 
 print "Done"
