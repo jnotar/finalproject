30c89f070d4eecacb58ab4f78bd1a09b6539672f
Julia Notar <jnotar@ucla.edu>
2015-02-27 23:47:11 +0000

    Improve function: add file opening to function

--- download_gb.py
+++ download_gb.py
@@ -12,11 +12,10 @@ from Bio import Entrez
 
 Entrez.email = "jnotar@ucla.edu" #tell Entrez who you are
 
-#open my idlist from my saved file of GI #s
-idlist = open("echino_ids.txt").read() #open list & read into one string 
-idlist = re.findall(r"\'(\d+)\'", idlist) #get rid of other characters by cutting out each id and putting it in a separate entry in a list
-
-def function_gb(idlist): 
+def function_gb(org, gene): 
+	#open my idlist from my saved file of GI #s
+    idlist = open("{}_{}_ids.txt".format(org, gene)).read() #open list & read into one string 
+    idlist = re.findall(r"\'(\d+)\'", idlist) #get rid of other characters by cutting out each id and putting it in a separate entry in a list
     for id in idlist:
         file = open("{}.gb".format(id), "w")
         handle = Entrez.efetch(db = "nucleotide", id = "{}".format(id), rettype = "gb", retmode = "text")
@@ -24,6 +23,6 @@ def function_gb(idlist):
         file.write(handle.read()) #write to file object
         file.close() #close file
 
-function_gb(idlist) #run the function on the idlist of GIs
+function_gb("Echinodermata", "opsin") #run the function on the idlist of GIs
 
 print "Done"
