49fe0a2b371108382b0cb5196bddb20b3985ae8d
Julia Notar <jnotar@ucla.edu>
2015-03-20 04:55:44 +0000

    Bash script that removes line breaks from all-in-one fasta file

---
+++ singlelines.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#uses awk program to take line breaks/wraps out of fasta file
+awk '/^>/{print s? s"\n"$0:$0;s="";next}{s=s sprintf("%s",$0)}END{if(s)print s}' echino_sequences.fasta > new.fasta
+
+#rewrites improved stuff to original file for ease of file names
+cat new.fasta > echino_sequences.fasta
