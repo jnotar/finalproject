151003c39647bad55ad3e6fae03d5d08ed2a71d1
Julia Notar <jnotar@ucla.edu>
2015-03-20 05:49:40 +0000

    Alignment script

---
+++ align.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+from __future__ import division
+
+import Bio
+from Bio.Align.Applications import ClustalwCommandline
+from Bio import AlignIO
+from Bio import Phylo
+
+#use clustalw to generate an alignment
+
+def align_seqs(filename):
+    cline = ClustalwCommandline("clustalw2", infile="{}.fasta".format(filename))
+    from Bio.Align.Applications import ClustalwCommandline
+    clustalw_exe = r"/users/jnotar/clustalw/clustalw2"
+    clustalw_cline = ClustalwCommandline(clustalw_exe, infile="{}.fasta".format(filename))
+    assert os.path.isfile(clustalw_exe), "Clustal W executable missing"
+    stdout, stderr = clustalw_cline()
+    align = AlignIO.read("{}.aln".format(filename), "clustal")
