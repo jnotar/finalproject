6d10cd540fe177e488155c545c1b6e31159a2fdb
Julia Notar <jnotar@ucla.edu>
2015-03-21 02:23:12 +0000

    Updated version of  NCBI download script

--- db_download.py
+++ db_download.py
@@ -5,25 +5,40 @@ from __future__ import division
 import pexpect
 
 def function(db, length):
-	for num in range(int(length)):
+	for num in range(length):
+		#turn length (e.g. 3) into a a range 00, 01, 02, 03
+		#this will match the format of the ncbi database files
+		if num < 10:
+			num = "0{}".format(num)
+		else: 
+			num = num
 		#initialize pexpect to bash
-		kid = pexpect.spawn("ftp ftp.ncbi.nlm.nih.gov")
+		kid = pexpect.spawn("ftp ftp.ncbi.nlm.nih.gov") #open the ftp server
+		#log in
 		kid.expect('Name .*:')
 		kid.sendline('anonymous')
 		kid.expect('Password:')
 		kid.sendline('jnotar@ucla.edu')
-		kid.expect("ftp> ")
+		#run a couple of steps as specified in the ncbi tutorial
+		kid.expect("ftp>")
 		kid.sendline("cd blast/db")
-		kid.expect("ftp> ")
+		kid.expect("ftp>")
 		kid.sendline("bin")
-		kid.expect("ftp> ")
-		kid.sendline("get {}.{}.tar.gz".format(str(db), str(num))
-		kid.expect("ftp> ")
-		kid.sendline("bye")
-		#kid.sendline("quit")
-		#tar xf db.num.tar.gz
-		#rm db.num.tar.gz
+		kid.expect("ftp>")
+		#ask to download the appropriate zipped database
+		kid.sendline("get {}.{}.tar.gz".format(db, num))
+		kid.expect("ftp> ", timeout=None) 
+		#timeout = none asks .expect() to wait until the process is done running
+		#close ftp
+		kid.sendline("bye") 
+		#kill process
+		kid.kill(0)
+		#unzip the file
+		pexpect.run("tar zxvpf {}.{}.tar.gz".format(db, num))
 
-function("nt", 3)
+#run the function for nucleotide database (nt), 00-25
+function("nt", 26)
 
+#Let me know that the script is done running
 print "Done!"
+
